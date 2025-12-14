323c76577b325745fb9078538af644be026f02d3
jagdep-singh <jagdeep.singh12a@gmail.com>
2025-12-15 02:43:06 +0530

    frontend dev changes

--- app/main.py
+++ app/main.py
@@ -1,13 +1,24 @@
-from operator import mod
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.api.auth import router as auth_router
 from app.api.sweets import router as sweets_router
 
-app = FastAPI(title="Incubyte Sweet Shop API")
+app = FastAPI(title="Sweet Shop API")
 
-app.include_router(auth_router)
-app.include_router(sweets_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth_router, prefix="/api/auth", tags=["auth"])
+app.include_router(sweets_router, prefix="/api/sweets", tags=["sweets"])
 
 @app.get("/status")
-def health():
-    return {"status": "running"}
+def status():
+    return {"status": "ok"}
